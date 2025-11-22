bad961f1b0e9708dfea01e6b403fc5e2cab4f285
Md-Siyam94 <mdsiyam9424@gmail.com>
2025-11-22 13:25:05 +0600

    set headears

--- todo app/server.js
+++ todo app/server.js
@@ -1,10 +1,36 @@
 const http = require('http');
 
-const server =  http.createServer((req, res)=>{
+const data = [
+    {
+        name: "Md Siyam",
+        age: 20,
+        getJob: false
+    },
+     {
+        name: " redo",
+        age: 20,
+        getJob: false
+    }
+]
+const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
-    res.end('Welcome to ToDo app')
-})
 
-server.listen(5000, '127.0.0.1', ()=>{
+    if (req.url === "/todos" && req.method === "GET") {
+        res.writeHead(200, {
+            "content-type": "text/json",
+            // "email" : "siyam@gmail.com"
+        })
+        // res.setHeaders({"content-type" , "text/json"})
+        return res.end(JSON.stringify(data));
+    }
+
+    if (req.url === "/todos/create-todo" && req.method === "POST") {
+        return res.end("data has created");
+    }
+
+    res.end("route not found");
+});
+
+server.listen(5000, "localhost", () => {
     console.log("server is running on port 5000");
-})
+});
