16af3ab20d0a7b1afc52a6799f47fafe9e835693
Md-Siyam94 <mdsiyam9424@gmail.com>
2025-11-26 11:40:33 +0600

    read the file

--- todo app/server.js
+++ todo app/server.js
@@ -1,23 +1,18 @@
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-const data = [
-    {
-        name: "Md Siyam",
-        age: 20,
-        getJob: false
-    },
-     {
-        name: " redo",
-        age: 20,
-        getJob: false
-    }
-]
+const filePath = path.join(__dirname, "./db/todo.json")
+// console.log(filePath);
+
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
     if (req.url === "/todos" && req.method === "GET") {
+        const data = fs.readFileSync(filePath, { encoding: "utf-8" })
+        // console.log(data);
         res.writeHead(200, {
-            "content-type": "text/json",
+            "content-type": "application/json",
             // "email" : "siyam@gmail.com"
         })
         // res.setHeaders({"content-type" , "text/json"})
